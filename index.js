eb83c944bdc86024ddb11c7a9d2e6d6cede6b997
John A <john.arena.3@gmail.com>
2025-10-31 10:57:19 -0500

    v3 init

--- index.js
+++ index.js
@@ -2,25 +2,16 @@
 
 const data = {
   lcp_manifest: require('./lib/lcp_manifest.json'),
-  // actions: require('./lib/actions.json'),
-  // backgrounds: require('./lib/backgrounds.json'),
   core_bonuses: require('./lib/core_bonuses.json'),
-  // environments: require('./lib/environments.json'),
   frames: require('./lib/frames.json'),
-  // manufacturers: require('./lib/manufacturers.json'),
-  // mods: require('./lib/mods.json'),
   pilot_gear: require('./lib/pilot_gear.json'),
   reserves: require('./lib/reserves.json'),
-  // sitreps: require('./lib/sitreps.json'),
-  // skills: require('./lib/skills.json'),
-  // statuses: require('./lib/statuses.json'),
   systems: require('./lib/systems.json'),
-  // tables: require('./lib/tables.json'),
-  // tags: require('./lib/tags.json'),
   talents: require('./lib/talents.json'),
   weapons: require('./lib/weapons.json'),
-  npc_features: require('./lib/npc_features.json'),
-  npc_templates: require('./lib/npc_templates.json'),
+  npct_industrial: require('./lib/npct_industrial.json'),
+  npct_specop: require('./lib/npct_specop.json'),
+  npct_horror: require('./lib/npct_horror.json'),
 };
 
 module.exports = data;
